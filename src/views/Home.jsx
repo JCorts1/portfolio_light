035d0918d85b8b0a30e19960b116a33a63e39a57
Jay Cortes <cortesjay1991@gmail.com>
2024-09-03 20:07:53 +0200

    Home and routes with react router dom has been created

---
+++ src/views/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/Home.css"
+
+const Home = () => {
+  return (
+    <div className='father-container'>
+      <div className='father-filter'>
+        <div className='card-info'>
+          <h1 className='myname'>I am Jay<span className='color-code1'>.Cortes</span></h1>
+          <h2><span className='color-code2'>("</span>Full_Stack: Web_developer<span className='color-code2'>")</span></h2>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
