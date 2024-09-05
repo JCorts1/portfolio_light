601263b521b22d0c3c11c7b0ca236e1a563722d1
Jay Cortes <cortesjay1991@gmail.com>
2024-09-05 20:09:55 +0200

    Tailwind is now working!

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from "path"
-import tailwindcss from "tailwindcss";
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 7030,
-  },
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
