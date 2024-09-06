33f69818eb1cf177438245841e517db142d578f9
Jay Cortes <cortesjay1991@gmail.com>
2024-09-06 17:07:43 +0200

    Found a bug from carousel where I was not using Capital letters

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Projects.css"
-import { CarouselOrientation } from "../components/carouselOrientation";
+import { CarouselOrientation } from "../components/CarouselOrientation";
 import Navbar from "../components/Navbar";
 
 const Projects = () => {
